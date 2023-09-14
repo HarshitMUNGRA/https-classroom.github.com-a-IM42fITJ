bb96d577824ced0cbef414396603818e17c4ce5a
Harshit MUNGRA <hrmungra69@gmail.com>
2023-09-14 11:58:19 -0300

    add authornameanddate

--- profitfromstock.js
+++ profitfromstock.js
@@ -2,3 +2,17 @@
 /*
 Author = Harshit Mungra;
 Date = 09/14/2023;
+Despriction = part 4 of exercise 2 in PROG1700
+A. Assign the value 10 to the variable purchase_price.
+B. Assign the value 15 to the variable selling_price.
+C. Assign to the variable percent_profit, 100 times the value of the difference between
+selling_price and purchase_price divided by purchase_price.
+D. Display the value of the variable percent_profit in the console window.
+*/
+function main()
+{
+    
+
+
+
+}
